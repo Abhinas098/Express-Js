7ffd07b278c484d967f8abc54cf17b69883d0c73
AbhinasDash <abhinasdash143@gmail.com>
2024-02-20 19:01:23 +0530

    handling routes and parsing req using body-parser

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-
 const express = require("express");
+
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name ="product"><button type="submit">Add</button></form>'
+  );
   console.log("in the middleware");
-  next(); //Its allow the request to continue to the next middleware
 });
-app.use((req, res, next) => {
-  console.log("in the next middleware");
-  res.send("<p>Hello There</p>"); //Express.js will automatically set the Content-Type header 
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body.product);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<p>Hello There</p>");
 });
 
 app.listen(3000);
